6bc4e1a3bbd1fe56c33940f5acc3b2bc839923f9
KauanPinheiro <kauan.pnascimento750@gmail.com>
2024-11-26 14:21:20 -0300

    quase pronto

--- js/script.js
+++ js/script.js
@@ -14,3 +14,30 @@ menu.addEventListener('click', () => {
 overlay.addEventListener('click', () => {
     menu.classList.remove('abrir-menu')
 })
+
+//Estilizando os efeitos de animaçôes  
+ScrollReveal().reveal('.txt-topo-site', {
+    origin: 'left',
+    duration: 2000,
+    distance: '20%'
+});
+ScrollReveal().reveal('.especialidades-box', {
+    origin: 'left',
+    duration: 2000,
+    distance: '20%'
+});
+ScrollReveal().reveal('.txt-sobre', {
+    origin: 'right',
+    duration: 2000,
+    distance: '20%'
+});
+ScrollReveal().reveal('.overlay', {
+    origin: 'left',
+    duration: 2000,
+    distance: '20%'
+});
+ScrollReveal().reveal('.form', {
+    origin: 'top',
+    duration: 2000,
+    distance: '20%'
+});
